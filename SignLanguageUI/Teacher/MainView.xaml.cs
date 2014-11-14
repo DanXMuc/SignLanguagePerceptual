da1675722047631f56d67816deaabaece7b2463d
barsuhn <bernd.barsuhn@web.de>
2014-11-14 16:05:34 +0100

    UI Basis

---
+++ SignLanguageUI/Teacher/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Teacher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+        }
+    }
+}
---
+++ SignLanguageUI/Teacher/SignGesture.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+
+namespace Teacher
+{
+    class SignGesture : PropertyChangedBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+    }
+}
