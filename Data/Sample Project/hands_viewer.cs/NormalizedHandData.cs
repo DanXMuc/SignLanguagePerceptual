93c054652992e5af41720e0bfacd8ae9ca0e13c5
Daniel Peter <peterd@in.tum.de>
2014-11-14 15:04:31 +0100

    add normalizedhanddata class

---
+++ Data/Sample Project/hands_viewer.cs/NormalizedHandData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace hands_viewer.cs
+{
+    class NormalizedHandData
+    {
+        public PXCMPoint3DF32 middleFingerPositionWorld;
+        public PXCMPoint3DF32 palmPositionWorld;
+
+        public PXCMPoint3DF32 middleFingerVelocity;
+        public PXCMPoint3DF32 palmVelocity;
+
+
+        NormalizedHandData(PXCMHandData.IHand handData) {
+            PXCMHandData.JointData j;
+            handData.QueryNormalizedJoint(PXCMHandData.JointType.JOINT_MIDDLE_TIP, out j);
+            middleFingerPositionWorld = j.positionWorld;
+            middleFingerVelocity = j.speed;
+            handData.QueryNormalizedJoint(PXCMHandData.JointType.JOINT_CENTER, out j);
+            palmPositionWorld = j.positionWorld;
+            palmVelocity = j.speed;
+        }
+    }
+}
